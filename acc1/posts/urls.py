f22a6ad02e128188a48d763faee4a0e43d648033
yeonnan <145152442+yeonnan@users.noreply.github.com>
2025-02-01 00:14:10 +0900

    게시글 수정, 삭제

--- acc1/posts/urls.py
+++ acc1/posts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListAPIView.as_view(), name='post-list'),
-    path('<int:post_id>/', views.PostDetailAPIView.as_view(), name='post-detail'),
+    path('<int:pk>/', views.PostDetailAPIView.as_view(), name='post-detail'),
 ]
--- acc1/posts/views.py
+++ acc1/posts/views.py
@@ -30,6 +30,35 @@ class PostListAPIView(APIView):
 class PostDetailAPIView(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    # 특정 pk에 해당하는 Post 객체 가져오고 없으면 404 반환
+    def get_object(self, pk):
+        try:
+            return get_object_or_404(Post, id=pk)
+        except Http404:
+            raise Http404('게시글을 찾을 수 없습니다.')
+
     # 게시글 상세 조회
     def get(self, request, pk):
-        pass
+        post = self.get_object(pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+    # 게시글 수정
+    def put(self, request, pk):
+        post = self.get_object(pk)
+
+        # 현재 로그인한 사용자와 게시글 작성자가 다를 경우 오류 반환
+        if post.user != request.user:
+            return Response({'message' : '유효하지 않은 사용자입니다.'}, status=400)
+        
+        serializer = PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        if post.user != request.user:
+            return Response({'message' : '유효하지 않은 사용자입니다.'}, status=400)
+        post.delete()
+        return Response({'message' : '삭제완료'}, status=200)
